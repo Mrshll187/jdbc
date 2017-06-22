a67359ce7b619f6dedddf633a163cad3b4bfa9ec
xxx <xxx@localhost.localdomain>
2017-06-21 22:15:58 -0400

    Formatted code

--- src/main/java/xxx/Testing.java
+++ src/main/java/xxx/Testing.java
@@ -10,28 +10,28 @@ import org.apache.commons.io.IOUtils;
 @SuppressWarnings("deprecation")
 public class Testing {
 
-	public static void main(String...args) throws Exception{
-		
+	public static void main(String... args) throws Exception {
+
 		Connection connection = DriverManager.getConnection("jdbc:h2:mem:test");
-		
-		String createTable = IOUtils.toString(Testing.class.getClassLoader().getResourceAsStream("create-db.sql"));		
+
+		String createTable = IOUtils.toString(Testing.class.getClassLoader().getResourceAsStream("create-db.sql"));
 		connection.createStatement().executeUpdate(createTable);
-		
+
 		String payload = IOUtils.toString(Testing.class.getClassLoader().getResourceAsStream("example.json"));
-		
+
 		String sql = "INSERT INTO events (id, name) VALUES (1, ?) ";
-		
-	    PreparedStatement statement = connection.prepareStatement(sql);
-	    statement.setString(1, payload);
-	    statement.executeUpdate();
-	    statement.close();
-		
+
+		PreparedStatement statement = connection.prepareStatement(sql);
+		statement.setString(1, payload);
+		statement.executeUpdate();
+		statement.close();
+
 		ResultSet results = connection.createStatement().executeQuery("SELECT * FROM events");
-		
-		while(results.next()){
-			
+
+		while (results.next()) {
+
 			System.out.println(results.getString("name"));
-			System.out.println("ID "+results.getLong("id"));
+			System.out.println("ID " + results.getLong("id"));
 		}
 	}
 }
