e8ff81a2000d5976ce528e27d762a2204d310bd7
xxx <xxx@localhost.localdomain>
2017-06-25 21:23:50 -0400

    Attempting with new SQL types

--- src/main/java/xxx/Testing.java
+++ src/main/java/xxx/Testing.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Date;
+import java.util.UUID;
 
 import org.apache.commons.io.IOUtils;
 
@@ -11,6 +13,7 @@ import org.apache.commons.io.IOUtils;
 public class Testing {
 
 	public static final String EVENT_TABLE = "EVENT";
+	
 	public static final String ID = "ID";
 	public static final String DATE = "DATE";
 	public static final String JSON = "JSON";
@@ -24,20 +27,36 @@ public class Testing {
 
 		String payload = IOUtils.toString(Testing.class.getClassLoader().getResourceAsStream("example.json"));
 
-		String sql = "INSERT INTO "+EVENT_TABLE+" ("+JSON+") VALUES (?) ";
+		UUID uuid = UUID.randomUUID();
+		long date = new Date().getTime();
+		
+		String sql = "INSERT INTO "+EVENT_TABLE+" ( " + ID + "," + JSON + ", "+DATE+") VALUES ("+uuid.toString()+", ? ," +date+ ") ";
 
 		PreparedStatement statement = connection.prepareStatement(sql);
 		statement.setString(1, payload);
 		statement.executeUpdate();
 		statement.close();
 
+		statement = connection.prepareStatement(sql);
+		statement.setString(1, payload);
+		statement.executeUpdate();
+		statement.close();
+		
 		ResultSet results = connection.createStatement().executeQuery("SELECT * FROM "+EVENT_TABLE+"");
-
+		
 		while (results.next()) {
 
-			System.out.println(JSON + " " + results.getString(JSON));
-			System.out.println(ID + " " + results.getLong(ID));
-			System.out.println(DATE + " " + results.getDate(DATE));
+			//System.out.println(JSON + " " + results.getString(JSON));
+			System.out.println(ID + " " + results.getString(ID));
+			System.out.println(DATE + " " + results.getLong(DATE));
 		}
+		
+		results = connection.createStatement().executeQuery("SELECT LAST_INSERT_ID()");
+		
+		while(results.next()){
+			
+			System.out.println(results.getString(1));
+		}
+		
 	}
 }
